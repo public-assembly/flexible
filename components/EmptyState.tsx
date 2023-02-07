0b04ddfa31358fb1a7b65f7807e6ed7c6957cad5
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-02-06 19:49:38 -0500

    feat: add empty state component

---
+++ components/EmptyState.tsx
@@ -0,0 +1,16 @@
+type EmptyState = {
+  actions?: React.ReactNode
+  heading: string
+  subheading?: string
+}
+
+export default function EmptyState(props: EmptyState) {
+  const { actions, heading, subheading } = props
+  return (
+    <div className="flex flex-col items-center h-full py-8">
+      <h2 className="mb-8 text-lg font-semibold text-center text-bold ">{heading}</h2>
+      {subheading && <p className="mb-4 text-md text-black/60">{subheading}</p>}
+      {actions && <div className="flex justify-center gap-2">{actions}</div>}
+    </div>
+  )
+}
