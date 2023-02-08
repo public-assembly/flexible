ec5a6014273d06aff9c6725fe8030ffcccf27085
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-02-07 20:59:15 -0500

    feat(css): add in css vars for theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
+      colors: {
+        current: 'currentColor',
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        background: 'hsl(var(--background) / 1)',
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        highlight: 'rgb(var(--color-highlight) / <alpha-value>)',
+        tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
+      },
+      borderRadius: {
+        'object': 'var(--border-radius-object)',
+        'button': 'var(--border-radius-button)',
+      },
       animation: {
       fakeFade: 'fakeAnimation 300ms cubic-bezier(0.23, 1, 0.32, 1)',
       fadeOut: 'fadeOut 300ms cubic-bezier(0.23, 1, 0.32, 1)',
@@ -10,6 +26,9 @@ module.exports = {
       fadeInUp: 'fadeInUp 300ms cubic-bezier(0.23, 1, 0.32, 1)',
       fadeInDown: 'fadeInDown 300ms cubic-bezier(0.23, 1, 0.32, 1)',
       fadeOutDown: 'fadeOutDown 300ms cubic-bezier(0.23, 1, 0.32, 1)',
+      longFadeInDown: 'longFadeInDown 500ms cubic-bezier(0.23, 1, 0.32, 1) forwards !important',
+      longFadeInUp: 'longFadeInUp 500ms cubic-bezier(0.23, 1, 0.32, 1) forwards !important',
+      longFadeInUp: 'longFadeInUp 500ms cubic-bezier(0.23, 1, 0.32, 1) forwards !important',
       },
       keyframes: {
         fadeInDown: {
@@ -102,4 +121,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
   ],
+    plugins: [
+    require("tailwindcss-radix")(),
+  ]
 }
