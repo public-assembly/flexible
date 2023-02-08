f5f822bd09f11de66e79bfdaec055b07f705cb11
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-02-07 21:25:54 -0500

    feat(utils): add hexToRgb and proposal url builder

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,1 +1,13 @@
 export const isServerSide = () => typeof window === 'undefined'
+
+const NOUNS_BUILDER_BASE_URL = 'https://nouns.build'
+
+export const buildCreateProposalUrl = (daoAddress: string) => new URL(`/dao/${daoAddress}/proposal/create`, NOUNS_BUILDER_BASE_URL).toString()
+
+export const hexToRgb = (hex: string) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `${r} ${g} ${b})`;
+}
