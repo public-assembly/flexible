18b3557174d2ab34963714b5c5316de45756e7a6
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-02-14 17:31:29 -0500

    fix(icons): set arrowupright default to text-primary

--- components/assets/icons/ArrowUpRight.tsx
+++ components/assets/icons/ArrowUpRight.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function ArrowUpRight({ size = 16, className = 'text-primary' }: { size: number; className?: string }) {
+function ArrowUpRight({ size = 16, className = 'text-primary' }: { size?: number; className?: string }) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
