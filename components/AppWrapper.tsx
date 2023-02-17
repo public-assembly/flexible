ad81cba1e70ee8c685702d4e599eab5b6c5c57c6
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-02-17 17:46:03 -0500

    refactor(AppWrapper): remove zdk

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -1,25 +1,18 @@
 import { SWRConfig } from 'swr'
-import { NFTFetchConfiguration } from '@zoralabs/nft-hooks'
-import { ZDKFetchStrategy } from '@zoralabs/nft-hooks/dist/strategies'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { TopProgressBar } from './TopProgressBar'
 
-export const strategy = new ZDKFetchStrategy('1', 'https://api.zora.co/graphql')
-
 export function AppWrapper({ children }: { children: JSX.Element }) {
   return (
-    <NFTFetchConfiguration networkId='1' strategy={strategy}>
-      <SWRConfig
-        value={{
-          fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <TopProgressBar />
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </SWRConfig>
-    </NFTFetchConfiguration>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <TopProgressBar />
+      <Header />
+      <main>{children}</main>
+    </SWRConfig>
   )
 }
