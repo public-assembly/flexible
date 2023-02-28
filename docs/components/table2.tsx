0692a8f1986956cd94ee8ddd13d4c5f20e05e99d
Valerie Andy <valdaraa@protonmail.com>
2023-02-28 17:13:49 -0500

    docs: replacing mdx tables w responsive tables

--- docs/components/table2.tsx
+++ docs/components/table2.tsx
@@ -1,6 +1,6 @@
 import styles from "./table.module.css";
 
-export function OptionTable({ options }: { options: [string, any] }) {
+export function OptionTable2({ options }: { options: [ URL, any] }) {
     return (
     <div
     className={
@@ -11,8 +11,8 @@ export function OptionTable({ options }: { options: [string, any] }) {
         <table className="w-full border-collapse text-sm">
         <thead>
             <tr className="border-b py-4 text-left dark:border-neutral-700">
-            <th className="py-2 font-semibold">Name</th>
-            <th className="py-2 px-6 font-semibold">Value</th>
+            <th className="py-2 font-semibold">Hook</th>
+            <th className="py-2 px-6 font-semibold">Usage</th>
             </tr>
         </thead>
         <tbody className="align-baseline text-gray-900 dark:text-gray-100">
