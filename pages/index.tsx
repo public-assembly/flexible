7aecfc66f278bc59b5546575df1952674ae83f93
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-01 12:07:29 -0500

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,18 @@ import { Flex } from '@/components/base/Flex'
 import { Stack } from '@/components/base/Stack'
 import { Seo } from '@/components/Seo'
 import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
+import { useIsMobile } from '../hooks/useIsMobile'
 
 function Home() {
+  const { isMobile } = useIsMobile()
   return (
     <section id='index' className='h-full'>
       <Seo />
-      <Stack className='px-4 pt-24'>
-        <Flex className='justify-center w-full'>
+      <Stack className='px-4 pt-24 '>
+        <Flex className='relative justify-center w-full'>
           <Stack className='w-[690px] h-[690px] rounded-object border border-primary bg-default-auction bg-cover p-4'>
             <Flex className='gap-4'>
-              <Button variant='tertiary' size='icon' className='w-fit'>
+              <Button variant='tertiary' className='w-fit'>
                 <ArrowLeftIcon />
               </Button>
               <Button variant='tertiary' className='w-fit'>
@@ -21,13 +23,21 @@ function Home() {
               </Button>
             </Flex>
           </Stack>
+          {isMobile ? null : (
+            <Button variant='tertiary' className='max-w-[133px] uppercase absolute right-0 top-10'>
+              <ArrowLeftIcon className='mr-2' />
+              Auction
+            </Button>
+          )}
         </Flex>
-        <Button variant='tertiary' prefix={<ArrowLeftIcon className='mr-2' />} className='max-w-[133px] uppercase'>
-          Auction
-        </Button>
-        <Button variant='tertiary' prefix={<ArrowUp className='mr-2' />} className='max-w-[133px] uppercase'>
-          Auction
-        </Button>
+
+        {/* Mobile auction button */}
+        {isMobile ? (
+          <Button variant='tertiary' className='max-w-[133px] uppercase'>
+            <ArrowUp className='mr-2' />
+            Auction
+          </Button>
+        ) : null}
       </Stack>
     </section>
   )
