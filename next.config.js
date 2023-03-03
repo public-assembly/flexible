ecfe90f42ebd30eb95d6ded80a25a48beae9b43b
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-02 19:38:52 -0500

    build(next.config.js): add transpilePackages and zora remote
    * removes deprecated transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['@public-assembly/dao-utils'])
-
 const nextConfig = {
+  transpilePackages: ['@public-assembly/dao-utils'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     esmExternals: 'loose',
+
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.zora.co',
+        port: '',
+      },
+    ],
   },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
