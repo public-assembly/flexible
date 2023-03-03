59e34a77d4da9d8dc27c7b793b37fe4d55fe2c89
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-02 20:59:42 -0500

    feat(auction): add auction component
    * add useAuction hook

---
+++ hooks/useAuction.ts
@@ -0,0 +1,61 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useActiveAuction, useDaoToken } from '@public-assembly/dao-utils';
+import { useCallback, useEffect, useState } from 'react';
+import { ENV } from 'utils/env';
+
+export const useAuction = () => {
+  const { totalSupply } = useActiveAuction(ENV.TOKEN_ADDRESS)
+
+  const [tokenId, setTokenId] = useState(0)
+
+  useEffect(() => {
+    totalSupply && setTokenId(totalSupply - 1)
+  }, [totalSupply])
+
+  const incrementId = useCallback(() => {
+    if (totalSupply && tokenId < totalSupply - 1) {
+      setTokenId(tokenId + 1)
+    }
+  }, [setTokenId, tokenId])
+
+  const decrementId = useCallback(() => {
+    if (totalSupply && tokenId > 0) {
+      setTokenId(tokenId - 1)
+    }
+  }, [setTokenId, tokenId])
+
+  const isFirstToken = tokenId === 0
+  const isLastToken = tokenId === totalSupply - 1
+
+
+  const [thumbnail, setThumbnail] = useState<undefined | string>()
+  const [name, setName] = useState<undefined | string>()
+
+  const { tokenData } = useDaoToken({
+    tokenAddress: ENV.TOKEN_ADDRESS,
+    tokenId: tokenId,
+  })
+
+  useEffect(() => {
+    const image = tokenData?.metadata?.image
+    const name = tokenData?.metadata?.name
+    if (image) {
+      setThumbnail(image)
+    }
+    if (name) {
+      setName(name)
+    }
+  }, [tokenData])
+
+  return {
+    tokenId: tokenId.toString(),
+    incrementId,
+    decrementId,
+    totalSupply,
+    isFirstToken,
+    isLastToken,
+    thumbnail,
+    tokenName: name
+  }
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,13 @@
-import { ArrowUp } from '@/components/assets/icons'
-import Button from '@/components/base/Button'
-import { Flex } from '@/components/base/Flex'
-import { Stack } from '@/components/base/Stack'
+import Auction from '@/components/auction/Auction'
 import { Seo } from '@/components/Seo'
-import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
-import { useIsMobile } from '../hooks/useIsMobile'
 
-function Home() {
-  const { isMobile } = useIsMobile()
+function HomePage() {
   return (
-    <section id='index' className='h-full'>
+    <section id='index' className='h-screen pb-4 md:h-full'>
       <Seo />
-      <Stack className='px-4 pt-24 '>
-        <Flex className='relative justify-center w-full'>
-          <Stack className='w-[690px] h-[690px] rounded-object border border-primary bg-default-auction bg-cover p-4'>
-            <Flex className='gap-4'>
-              <Button variant='tertiary' className='w-fit'>
-                <ArrowLeftIcon />
-              </Button>
-              <Button variant='tertiary' className='w-fit'>
-                <ArrowRightIcon />
-              </Button>
-            </Flex>
-          </Stack>
-          {isMobile ? null : (
-            <Button variant='tertiary' className='max-w-[133px] uppercase absolute right-0 top-10'>
-              <ArrowLeftIcon className='mr-2' />
-              Auction
-            </Button>
-          )}
-        </Flex>
-
-        {/* Mobile auction button */}
-        {isMobile ? (
-          <Button variant='tertiary' className='max-w-[133px] uppercase'>
-            <ArrowUp className='mr-2' />
-            Auction
-          </Button>
-        ) : null}
-      </Stack>
+      <Auction />
     </section>
   )
 }
 
-export default Home
+export default HomePage
