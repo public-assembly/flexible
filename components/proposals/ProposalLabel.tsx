26121807aec40ce81a339f0bd14a688f1ea3751a
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-03 17:34:22 -0500

    style(ProposalLabel): fix the title casing

--- components/proposals/ProposalLabel.tsx
+++ components/proposals/ProposalLabel.tsx
@@ -1,46 +1,55 @@
-import { VariantProps, cva } from 'cva'
-import { forwardRef, ReactNode } from 'react'
+import { ReactNode, forwardRef } from "react"
 
-import { cn } from 'utils/cn'
+import { VariantProps, cva } from "cva"
+import { cn } from "utils/cn"
 
 const labelVariants = cva(
   [
-    'border-[.5px] rounded-[2px] w-fit px-1 py-[2px] lowercase  h-5 inline-flex items-center box-border',
-    'text-xs leading-4 font-medium',
+    "border-[.5px] rounded-[2px] w-fit px-1 py-[2px]  h-5 inline-flex items-center box-border",
+    "text-xs leading-4 font-medium",
   ],
   {
     variants: {
       variant: {
-        primary: ['bg-primary text-secondary'],
-        secondary: ['bg-secondary text-primary'],
-        tertiary: ['border-tertiary text-primary/50 bg-tertiary/50'],
+        primary: ["bg-primary text-secondary"],
+        secondary: ["bg-secondary text-primary"],
+        tertiary: ["border-tertiary text-primary/50 bg-tertiary/50"],
       },
-      textTransform: {
-        uppercase: 'uppercase',
-        lowercase: 'lowercase',
-        titlecase: 'lowercase first-letter:capitalize',
+      titleCase: {
+        true: "",
       },
     },
     defaultVariants: {
-      variant: 'primary',
-      textTransform: 'titlecase',
+      variant: "primary",
     },
   }
 )
 
-export type LabelProps = { className?: string; children: ReactNode } & VariantProps<typeof labelVariants>
+export type LabelProps = {
+  className?: string
+  children: ReactNode
+} & VariantProps<typeof labelVariants>
 
 const ProposalLabel = forwardRef<HTMLDivElement, LabelProps>(
-  ({ className, textTransform, variant, children, ...props }, ref) => {
+  ({ className, titleCase, variant, children, ...props }, ref) => {
     return (
-      <div className={cn(className, labelVariants({ variant, textTransform }))} ref={ref} {...props}>
-        {children}
+      <div
+        className={cn(className, labelVariants({ variant, titleCase }))}
+        ref={ref}
+        {...props}
+      >
+        {/* I need to wrap in a span for some reason the styles for titlecase won't apply otherwise */}
+        {titleCase ? (
+          <span className="lowercase first-letter:capitalize">{children}</span>
+        ) : (
+          children
+        )}
       </div>
     )
   }
 )
 
-ProposalLabel.displayName = 'ProposalLabel'
+ProposalLabel.displayName = "ProposalLabel"
 
 export { ProposalLabel, labelVariants }
 export default ProposalLabel
