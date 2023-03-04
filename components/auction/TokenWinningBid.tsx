67f8f05b5968d009c0232ff97903b1097e507c8f
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-04 03:05:00 -0500

    feat(auction): add prev auction bidders and eth links
    fixes the styling with label having captions too

--- components/auction/TokenWinningBid.tsx
+++ components/auction/TokenWinningBid.tsx
@@ -1,7 +1,18 @@
-import * as React from 'react'
-import { ethers } from 'ethers'
-import { useActiveAuction, useNounsProtocol, useDaoToken, etherscanLink } from '@public-assembly/dao-utils'
-import { Flex } from '@/components/base/Flex'
+import * as React from "react"
+
+import {
+  etherscanLink,
+  useActiveAuction,
+  useDaoToken,
+  useNounsProtocol,
+} from "@public-assembly/dao-utils"
+import { ethers } from "ethers"
+import { shortenAddress } from "utils/shortenAddress"
+import { useEnsName } from "wagmi"
+
+import { Flex } from "@/components/base/Flex"
+import { Caption } from "@/components/base/Typography"
+import { buildEtherscanAddressLink } from "../../utils/helpers"
 
 export type AuctionEvent = {
   id: number
@@ -10,7 +21,13 @@ export type AuctionEvent = {
   transactionHash: string
 }
 
-export const TokenWinningBid = ({ tokenId, tokenAddress }: { tokenAddress: `0x${string}`; tokenId: string }) => {
+export const TokenWinningBid = ({
+  tokenId,
+  tokenAddress,
+}: {
+  tokenAddress: `0x${string}`
+  tokenId: string
+}) => {
   const { auctionData } = useActiveAuction(tokenAddress)
 
   const { tokenData } = useDaoToken({
@@ -23,8 +40,18 @@ export const TokenWinningBid = ({ tokenId, tokenAddress }: { tokenAddress: `0x${
     auctionAddress: auctionData?.address,
   })
 
-  const [winningBid, setWinningBid] = React.useState<string | undefined>('N/A')
+  const [winningBid, setWinningBid] = React.useState<string | undefined>("N/A")
   const [winningTx, setWinningTx] = React.useState<string | undefined>()
+  const [address, setAddress] = React.useState<string | undefined>()
+
+  const { data: ensName } = useEnsName({
+    address: address as `0x${string}` | undefined,
+  })
+
+  const bidder = React.useMemo(
+    () => (ensName ? ensName : shortenAddress(address)),
+    [ensName, address]
+  )
 
   React.useEffect(() => {
     async function getBids() {
@@ -36,9 +63,9 @@ export const TokenWinningBid = ({ tokenId, tokenAddress }: { tokenAddress: `0x${
            * https://github.com/ourzora/nouns-protocol/blob/main/src/auction/IAuction.sol#L16-L22
            */
           const bids = await auctionContract?.queryFilter(
-            'AuctionBid' as any,
+            "AuctionBid" as any,
             tokenData?.mintInfo?.mintContext?.blockNumber,
-            'latest' /* Clamp at next token block number if decrementing */
+            "latest" /* Clamp at next token block number if decrementing */
           )
           if (bids) {
             const auctionEventsArray = bids.map((event: any) => {
@@ -50,47 +77,77 @@ export const TokenWinningBid = ({ tokenId, tokenAddress }: { tokenAddress: `0x${
               }
             }) as AuctionEvent[]
 
-            const tokenEvents = auctionEventsArray?.filter((token) => token?.id === Number(tokenId))
+            const tokenEvents = auctionEventsArray?.filter(
+              (token) => token?.id === Number(tokenId)
+            )
 
             if (tokenEvents?.length) {
               const lastTokenEvent = tokenEvents.at(-1)
-              setWinningBid(`${lastTokenEvent?.amount} ETH`)
-              setWinningTx(etherscanLink({ hash: lastTokenEvent?.transactionHash }))
+              setAddress(lastTokenEvent?.bidder)
+              setWinningBid(`${lastTokenEvent?.amount}`)
+              setWinningTx(
+                etherscanLink({ hash: lastTokenEvent?.transactionHash })
+              )
             } else {
-              setWinningBid('N/A')
+              setWinningBid("N/A")
               setWinningTx(undefined)
             }
           }
         }
-      } catch (err) {
-        // console.error(err)
-      }
+      } catch (err) {}
     }
     getBids()
 
-    return function cleanup() {
-      /**
-       * Short circuit the async call:
-       * https://stackoverflow.com/questions/37624144/is-there-a-way-to-short-circuit-async-await-flow
-       */
-      // console.log('unmount')
-    }
+    return function cleanup() {}
   }, [auctionContract, tokenId, tokenData])
 
-  return (
-    <Flex className='z-10 px-4 py-2 leading-5 bg-primary text-secondary rounded-object w-fit'>
-      <Flex>
-        <span>Winning bid</span>
-        <span className='px-1'> Ξ </span>
+  if (!!bidder && !!address)
+    return (
+      <Flex className="z-10 items-center gap-4 ">
+        <div className="px-4 py-2 bg-primary text-secondary rounded-object w-fit">
+          <a
+            href={winningTx}
+            target="_blank"
+            rel="noreferrer"
+            className={`${
+              !winningTx && "pointer-events-none"
+            }  h-6 inline-flex items-center group`}
+          >
+            <Caption className="uppercase text-secondary">
+              Ξ <span className="group-hover:underline">{winningBid}</span>
+            </Caption>
+          </a>
+        </div>
+        <a
+          href={buildEtherscanAddressLink(address)}
+          target="_blank"
+          rel="noreferrer"
+          className={`${
+            !winningTx && "pointer-events-none"
+          }  h-6 inline-flex items-center group`}
+        >
+          <div className="px-4 py-2 bg-primary text-secondary rounded-object w-fit">
+            {bidder}
+          </div>
+        </a>
       </Flex>
+    )
+
+  return (
+    <Flex className="z-10 items-center gap-4 px-4 py-2 bg-primary text-secondary rounded-object w-fit">
+      <span className="pr-">Winning bid </span>
 
       <a
         href={winningTx}
-        target='_blank'
-        rel='noreferrer'
-        className={`${!winningTx && 'pointer-events-none'} hover:underline`}
+        target="_blank"
+        rel="noreferrer"
+        className={`${
+          !winningTx && "pointer-events-none"
+        }  h-6 inline-flex items-center group`}
       >
-        {winningBid}
+        <Caption className="uppercase text-secondary">
+          Ξ <span className="group-hover:underline">{winningBid}</span>
+        </Caption>
       </a>
     </Flex>
   )
