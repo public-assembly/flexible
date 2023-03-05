e71dff33aad584bd1e6c02469660ced156fb8fd4
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-04 21:04:28 -0500

    feat(proposal-card): add needs action logic

--- components/proposals/ProposalCard.tsx
+++ components/proposals/ProposalCard.tsx
@@ -1,7 +1,9 @@
 /* @ts-ignore */
 import * as React from "react"
+import { useEffect } from "react"
 
 import Link from "next/link"
+import { useAuth } from "@/hooks/useAuth"
 import Balancer from "react-wrap-balancer"
 import { cn } from "utils/cn"
 
@@ -17,6 +19,19 @@ import { ProposalCardVotes } from "./ProposalCardVotes"
 import ProposalLabel from "./ProposalLabel"
 
 export default function ProposalCard({ proposal }) {
+  const { address } = useAuth()
+  const [needsAction, setNeedsAction] = React.useState<boolean>(false)
+
+  useEffect(() => {
+    if (!address) return
+    const proposalVotes = proposal.votes
+
+    // Check if the current address has voted on this proposal.
+    const hasVoted = proposalVotes.some((vote) => vote.voter === address)
+
+    setNeedsAction(hasVoted)
+  }, [address, proposal.votes])
+
   return (
     <Link href={`proposals/${proposal.proposalId}`}>
       <Stack
@@ -27,15 +42,15 @@ export default function ProposalCard({ proposal }) {
         )}
       >
         {/* Statuses */}
-        <Flex className="items-center self-stretch justify-between gap-2 ">
+        <Flex className="flex-wrap items-center self-stretch justify-between gap-2">
           <Flex className="gap-2">
             <ProposalLabel>{proposal.status}</ProposalLabel>
-            <ProposalLabel>Needs vote</ProposalLabel>
+            {needsAction ? <ProposalLabel>Needs vote</ProposalLabel> : null}
           </Flex>
           <ProposalTimestamp voteStart={proposal.voteStart} size="xs" />
         </Flex>
 
-        <Stack className="gap-2">
+        <Stack className="h-full gap-2 ">
           <ProposalTitle title={proposal.title} />
 
           <span className="text-xs font-medium text-primary/50">
