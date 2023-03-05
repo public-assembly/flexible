e02d4b4340a1a4f8ce444b117b94a3f35860e9b1
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2023-03-05 02:55:33 -0500

    feat(useIsMobile): add user agent checks

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -2,6 +2,25 @@ import { useState, useEffect } from 'react'
 
 const MOBILE_WIDTH = 769
 
+export function isAndroid(): boolean {
+  return (
+    typeof navigator !== 'undefined' &&
+    /Android\s([0-9.]+)/.test(navigator.userAgent) // Source: https://github.com/DamonOehlman/detect-browser/blob/master/src/index.ts
+  );
+}
+
+export function isIOS(): boolean {
+  return (
+    typeof navigator !== 'undefined' &&
+    /Version\/([0-9._]+).*Mobile.*Safari.*/.test(navigator.userAgent) // Source: https://github.com/DamonOehlman/detect-browser/blob/master/src/index.ts
+  );
+}
+
+export function isMobile(): boolean {
+  return isAndroid() || isIOS();
+}
+
+
 export const useIsMobile = () => {
   const [isMobileWidth, setIsMobileWidth] = useState<boolean | null>(null)
   const [, setWidth] = useState<number>(0)
@@ -28,6 +47,6 @@ export const useIsMobile = () => {
   }, [])
 
   return {
-    isMobile: isMobileWidth,
+    isMobile: isMobileWidth || isMobile(),
   }
 }
