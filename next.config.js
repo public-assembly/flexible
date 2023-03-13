8192534ebc07402405229711356f5e0c2410ec98
Salief Lewis <salieflewis@gmail.com>
2023-03-13 10:22:02 -0400

    add new next config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,10 @@
-/** @type {import('next').NextConfig} */
-
-const withTM = require('next-transpile-modules')(['@public-assembly/dao-utils'])
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig = {
-  // transpilePackages: ['@public-assembly/dao-utils'],
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    esmExternals: 'loose',
-
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.zora.co',
-        port: '',
-      },
-    ],
-  },
+  // reactStrictMode: true,
+  transpilePackages: ['@public-assembly/dao-utils'],
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
