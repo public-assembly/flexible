59c5c4a1e829b8abaccf0ef26fcd8d4cd2957e8e
Salief Lewis <salieflewis@gmail.com>
2023-03-17 12:26:48 -0400

    update voting modal with available votes

--- components/proposals/ProposalVoteButton.tsx
+++ components/proposals/ProposalVoteButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import {
   Dialog,
   DialogContent,
@@ -7,48 +7,67 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '../base/Dialog'
-import { Check, Minus } from '../assets/icons'
-import { Stack } from '../base/Stack'
-import { Button } from '../base/Button'
+} from "../base/Dialog"
+import { Check, Minus, Exit } from "../assets/icons"
+import { Stack } from "../base/Stack"
+import { Button } from "../base/Button"
+import { useContractRead } from "wagmi"
+import { governorAbi, useGovernorContext } from "@public-assembly/dao-utils"
+import { useAuth } from "@/hooks/useAuth"
+import { BigNumber } from "ethers"
 
-const ProposalVoteButton = () => {
-  // todo pull in total vote count
-  const availableVotes = 1
+const ProposalVoteButton = ({ proposal }) => {
+  const { address } = useAuth()
+  const { governorAddress } = useGovernorContext()
+  const { data: availableVotes } = useContractRead({
+    address: governorAddress,
+    abi: governorAbi,
+    functionName: "getVotes",
+    args: [address as string, BigNumber.from(proposal?.timeCreated)],
+  })
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className='max-w-[327px]' size='lg'>
+        <Button className="max-w-[327px]" size="lg">
           Submit vote
         </Button>
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[425px]'>
+      <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Voting</DialogTitle>
           <DialogDescription>
-            Votes are permanent and you can&apos;t change votes after its submitted onchain. (will finalize).
+            Votes are permanent and cannot be altered after being submitted
+            onchain.
           </DialogDescription>
         </DialogHeader>
-        <Stack className='grid gap-4 py-6'>
-          <Button className='items-center w-full'>
+        <Stack className="grid gap-4 py-6">
+          <Button variant="tertiary" className="items-center w-full gap-x-1">
             <Check />
-            {availableVotes} vote for
+            {availableVotes?.toString()} vote for
           </Button>
-          <Button className='items-center w-full'>
-            <Check />
-            {availableVotes} vote against
+          <Button variant="tertiary" className="items-center w-full gap-x-1">
+            <Exit />
+            {availableVotes?.toString()} vote against
           </Button>
-          <Button className='items-center w-full'>
+          <Button variant="tertiary" className="items-center w-full gap-x-1">
             <Minus />
             Abstain from voting
           </Button>
         </Stack>
         <Stack>
-          <textarea placeholder='Comment(optional)' className='w-full min-h-[167px] text-black'></textarea>
+          <textarea
+            placeholder="Comment(optional)"
+            className="py-2 px-3 w-full min-h-[167px] text-black"
+          ></textarea>
         </Stack>
         <DialogFooter>
-          <Button type='submit' className='w-full'>
-            Save vote
+          <Button
+            variant="tertiary"
+            type="submit"
+            className="w-full py-8 lg:py-7"
+          >
+            Submit vote
           </Button>
         </DialogFooter>
       </DialogContent>
