a4b78037a47760bfe407890d269a38b1f063e0b8
Salief <salieflewis@gmail.com>
2023-03-21 15:34:31 -0400

    add wait for txn to save button

--- components/Save.tsx
+++ components/Save.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
 import { Web3Storage } from "web3.storage"
 import { useThemeContext } from "../context/ThemeProvider"
-import { usePrepareContractWrite, useContractWrite } from "wagmi"
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi"
 import { platformThemeRegistryAbi } from "../abi/platformThemeRegistryAbi"
 import { BigNumber } from "ethers"
 import { ENV } from "@/utils/env"
+import { Pending } from "./assets/icons"
 
 function getAccessToken() {
   return ENV.WEB3STORAGE_TOKEN
@@ -23,11 +28,10 @@ const platformIndex = process.env.NEXT_PUBLIC_PLATFORM_INDEX as number
 
 export function Save() {
   const { newMetadata } = useThemeContext()
-
   const [uri, setUri] = React.useState<string>("")
   const [themeReady, setThemeReady] = React.useState<boolean>(false)
 
-  const { config, error } = usePrepareContractWrite({
+  const { config } = usePrepareContractWrite({
     address: themeRegistry,
     abi: platformThemeRegistryAbi,
     functionName: "setPlatformTheme",
@@ -38,7 +42,11 @@ export function Save() {
     },
   })
 
-  const { write: setTheme } = useContractWrite(config)
+  const { data, write: setTheme } = useContractWrite(config)
+
+  const { isLoading } = useWaitForTransaction({
+    hash: data?.hash,
+  })
 
   React.useEffect(() => {
     if (setTheme && themeReady) {
@@ -69,10 +77,11 @@ export function Save() {
   return (
     <div className="flex justify-center bg-white w-full p-4">
       <button
+        disabled={isLoading}
         className="bg-[#121212] hover:bg-[#121212]/50 active:bg-[#121212] dark:bg-[#121212] dark:text-[#121212] text-[#f2fdf7] text-lg py-3 font-['Satoshi'] rounded w-full"
         onClick={handleClick}
       >
-        Save
+        {!isLoading ? "Save" : <Pending className="animate-spin" />}
       </button>
     </div>
   )
