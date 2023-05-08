9a01ad279621dba1252c881ccc08fff323aac63b
Salief <salieflewis@gmail.com>
2023-05-08 15:22:49 -0400

    update app file for connectkit

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "@/hooks/useAuth"
-import { ConnectButton as RKConnectButton } from "@rainbow-me/rainbowkit"
+import { ConnectKitButton, useModal } from "connectkit"
+// openSwitchNetworks, useChains
 import { Zorb } from "./base/Zorb"
 import { Button } from "@/components/base/Button"
 import { ENV } from "@/utils/env"
@@ -11,41 +12,39 @@ export default function ConnectButton() {
   const { address, ensName } = useAuth()
 
   return (
-    <RKConnectButton.Custom>
-      {({ chain, mounted, account, openConnectModal, openChainModal }) => {
+    <ConnectKitButton.Custom>
+      {/* {({ chain, mounted, account, openConnectModal, openChainModal }) => { */}
+
+      {({ isConnected, isConnecting, show, hide, address }) => {
+        // const { open, openSwitchNetworks } = useModal()
+        // const chains = useChains()
+
         return (
-          <>
-            {(() => {
-              if (!mounted || !account || !chain) {
-                return (
-                  <Button size="lg" onClick={openConnectModal}>
-                    Connect Wallet
-                  </Button>
-                )
-              }
-              if (chain?.id !== ENV.CHAIN) {
-                return (
-                  <Button
-                    size="md"
-                    icon="left"
-                    className="border border-black text-[#FF0000] bg-white hover:bg-white/50"
-                    onClick={openChainModal}
-                  >
-                    <Error className="mr-[6px]" />
-                    Switch Network
-                  </Button>
-                )
-              }
-              return (
-                <Flex className="items-center gap-4">
-                  <Zorb address={address} size={32} radius={999} />
-                  <Headline>{ensName ? ensName : address}</Headline>
-                </Flex>
-              )
-            })()}
-          </>
+          <Button size="lg" onClick={show}>
+            Connect Wallet
+          </Button>
         )
+
+        // if (chains?.id !== ENV.CHAIN) {
+        //   return (
+        //     <Button
+        //       size="md"
+        //       icon="left"
+        //       className="border border-black text-[#FF0000] bg-white hover:bg-white/50"
+        //       onClick={show}
+        //     >
+        //       <Error className="mr-[6px]" />
+        //       Switch Network
+        //     </Button>
+        //   )
+        // }
+        // return (
+        //   <Flex className="items-center gap-4">
+        //     <Zorb address={address} size={32} radius={999} />
+        //     <Headline>{ensName ? ensName : address}</Headline>
+        //   </Flex>
+        // )
       }}
-    </RKConnectButton.Custom>
+    </ConnectKitButton.Custom>
   )
 }
