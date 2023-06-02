90f84a9e3bf7815f23d3b10376452efe130e2d73
Salief <salieflewis@gmail.com>
2023-06-02 13:39:50 -0400

    add voting success modal, resolves #176

--- components/proposals/VotingDialog.tsx
+++ components/proposals/VotingDialog.tsx
@@ -22,15 +22,19 @@ import {
 } from '../base/Dialog'
 import { Stack } from '../base/Stack'
 import { BodyLarge, Headline } from '../base/Typography'
+import VotingSuccess from './VotingSuccess'
 
 const VotingDialog = ({ proposal }) => {
+  const [open, setOpen] = useState(false)
   const [support, setSupport] = useState<0 | 1 | 2 | undefined>()
   const [reason, setReason] = useState<string | undefined>()
   const [activeButton, setActiveButton] = useState<number>()
 
   const {
     castVote,
+    castVoteSuccess,
     castVoteWithReason,
+    castVoteWithReasonSuccess,
     castVoteLoading,
     castVoteWithReasonLoading,
   } = useVote({
@@ -48,116 +52,126 @@ const VotingDialog = ({ proposal }) => {
     args: [address as Hash, BigNumber.from(proposal?.timeCreated)],
   })
 
+  function renderSuccessDialog() {
+    if (castVoteSuccess || castVoteWithReasonSuccess) {
+      setOpen(false)
+      return <VotingSuccess reason={reason} />
+    }
+  }
+
   if (proposal.status != 'ACTIVE') return null
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="min-w-[328px]" size="lg">
-          Submit vote
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="gap-y-6 border border-primary sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>
-            <Headline>Voting</Headline>
-          </DialogTitle>
-          <DialogDescription>
-            Votes are permanent and cannot be altered after being submitted
-            onchain.
-          </DialogDescription>
-        </DialogHeader>
-        <Stack className="grid gap-4">
-          {/* Vote for */}
-          <button
-            onClick={() => {
-              setSupport(1)
-              setActiveButton(1)
-            }}
-            className={cn([
-              'flex h-12 w-full items-center justify-center gap-x-1',
-              'rounded-lg border border-primary/50 bg-secondary',
-              'hover:bg-primary hover:text-secondary',
-              activeButton == 1 && 'bg-primary text-secondary',
-            ])}
-          >
-            <Check />
-            {availableVotes?.toString()} vote for
-          </button>
-          {/* Vote against */}
-          <button
-            onClick={() => {
-              setSupport(0)
-              setActiveButton(0)
-            }}
-            className={cn([
-              'flex h-12 w-full items-center justify-center gap-x-1',
-              'rounded-lg border border-primary/50 bg-secondary',
-              'hover:bg-primary hover:text-secondary',
-              activeButton == 0 && 'bg-primary text-secondary',
-            ])}
-          >
-            <Exit />
-            {availableVotes?.toString()} vote against
-          </button>
-          {/* Abstain from voting */}
-          <button
-            onClick={() => {
-              setSupport(2)
-              setActiveButton(2)
-            }}
-            className={cn([
-              'flex h-12 w-full items-center justify-center gap-x-1',
-              'rounded-lg border border-primary/50 bg-secondary',
-              'hover:bg-primary hover:text-secondary',
-              activeButton == 2 && 'bg-primary text-secondary',
-            ])}
-          >
-            <Minus />
-            Abstain from voting
-          </button>
-        </Stack>
-        <Stack className="gap-y-2">
-          <BodyLarge>Reason for vote</BodyLarge>
-          <textarea
-            value={reason}
-            onChange={(e) => setReason(e.target.value)}
-            placeholder="Comment (optional)"
-            className="min-h-[96px] w-full py-2 px-3 text-black"
-          ></textarea>
-        </Stack>
-        <DialogFooter>
-          {!reason ? (
-            <Button
-              onClick={() => castVote?.()}
-              size="lg"
-              type="submit"
-              className="w-full"
-              disabled={support === undefined || support === null}
+    <>
+      {renderSuccessDialog()}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="min-w-[328px]" size="lg">
+            Submit vote
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="gap-y-6 border border-primary sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>
+              <Headline>Voting</Headline>
+            </DialogTitle>
+            <DialogDescription>
+              Votes are permanent and cannot be altered after being submitted
+              onchain.
+            </DialogDescription>
+          </DialogHeader>
+          <Stack className="grid gap-4">
+            {/* Vote for */}
+            <button
+              onClick={() => {
+                setSupport(1)
+                setActiveButton(1)
+              }}
+              className={cn([
+                'flex h-12 w-full items-center justify-center gap-x-1',
+                'rounded-lg border border-primary/50 bg-secondary',
+                'hover:bg-primary hover:text-secondary',
+                activeButton == 1 && 'bg-primary text-secondary',
+              ])}
             >
-              {!castVoteLoading ? (
-                'Submit vote'
-              ) : (
-                <Pending className="animate-spin" />
-              )}
-            </Button>
-          ) : (
-            <Button
-              onClick={() => castVoteWithReason?.()}
-              size="lg"
-              type="submit"
-              className="w-full"
-              disabled={support === undefined || support === null}
+              <Check />
+              {availableVotes?.toString()} vote for
+            </button>
+            {/* Vote against */}
+            <button
+              onClick={() => {
+                setSupport(0)
+                setActiveButton(0)
+              }}
+              className={cn([
+                'flex h-12 w-full items-center justify-center gap-x-1',
+                'rounded-lg border border-primary/50 bg-secondary',
+                'hover:bg-primary hover:text-secondary',
+                activeButton == 0 && 'bg-primary text-secondary',
+              ])}
             >
-              {!castVoteWithReasonLoading ? (
-                'Submit vote with reason'
-              ) : (
-                <Pending className="animate-spin" />
-              )}
-            </Button>
-          )}
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+              <Exit />
+              {availableVotes?.toString()} vote against
+            </button>
+            {/* Abstain from voting */}
+            <button
+              onClick={() => {
+                setSupport(2)
+                setActiveButton(2)
+              }}
+              className={cn([
+                'flex h-12 w-full items-center justify-center gap-x-1',
+                'rounded-lg border border-primary/50 bg-secondary',
+                'hover:bg-primary hover:text-secondary',
+                activeButton == 2 && 'bg-primary text-secondary',
+              ])}
+            >
+              <Minus />
+              Abstain from voting
+            </button>
+          </Stack>
+          <Stack className="gap-y-2">
+            <BodyLarge>Reason for vote</BodyLarge>
+            <textarea
+              value={reason}
+              onChange={(e) => setReason(e.target.value)}
+              placeholder="Comment (optional)"
+              className="min-h-[96px] w-full py-2 px-3 text-black"
+            ></textarea>
+          </Stack>
+          <DialogFooter>
+            {!reason ? (
+              <Button
+                onClick={() => castVote?.()}
+                size="lg"
+                type="submit"
+                className="w-full"
+                disabled={support === undefined || support === null}
+              >
+                {!castVoteLoading ? (
+                  'Submit vote'
+                ) : (
+                  <Pending className="animate-spin" />
+                )}
+              </Button>
+            ) : (
+              <Button
+                onClick={() => castVoteWithReason?.()}
+                size="lg"
+                type="submit"
+                className="w-full"
+                disabled={support === undefined || support === null}
+              >
+                {!castVoteWithReasonLoading ? (
+                  'Submit vote with reason'
+                ) : (
+                  <Pending className="animate-spin" />
+                )}
+              </Button>
+            )}
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
 
