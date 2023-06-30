5254c23f6a94c75f11c93e20e847d08afb099116
Salief <salieflewis@gmail.com>
2023-06-30 17:11:36 -0400

    adjust error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,9 @@
 'use client' // Error components must be Client Components
- 
+
+import { useAuctionState } from '@public-assembly/builder-utils'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
- 
+
 export default function Error({
   error,
   reset,
@@ -9,22 +11,10 @@ export default function Error({
   error: Error
   reset: () => void
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  )
+  const router = useRouter()
+  const { auctionState } = useAuctionState()
+
+  useEffect(() => router.push(`/${auctionState.tokenId}`), [])
+
+  return
 }
