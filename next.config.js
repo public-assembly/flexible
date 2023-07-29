b009f1f087b77e8996f857ae110d7971f074bf47
0xTranqui <thinkandchill11@gmail.com>
2023-07-29 21:55:21 +0200

    added in extra webpack code in next.config. still not working

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config
   },
   reactStrictMode: true,
